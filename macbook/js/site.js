32ff11a2c600b112ce6af9b65f37dc1e2db27c60
Conrad Gülzow <conrad.guelzow@basilicom.de>
2014-07-19 19:30:48 +0200

    asfsf

--- macbook/js/site.js
+++ macbook/js/site.js
@@ -10,6 +10,7 @@ App.tools = {
             click:function(e){
                 e.preventDefault();
                 var src = $(this).attr("src");
+                $(this).parent().append("<div class='over'></div>");
                 $(".fullImage img").attr("src",src);
             }
         });
